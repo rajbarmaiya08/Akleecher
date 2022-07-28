01c78ee37014ffd3248bd8ab0abe83e2be163ccb
rajbarmaiya08 <106380134+rajbarmaiya08@users.noreply.github.com>
2022-07-28 15:22:03 +0530

    Update __main__.py

--- bot/__main__.py
+++ bot/__main__.py
@@ -75,14 +75,14 @@ def stats(update, context):
 
 def start(update, context):
     buttons = ButtonMaker()
-    buttons.buildbutton("Owner", "https://t.me/ak_nh4")
-    buttons.buildbutton("Mirror Group", "https://t.me/AK_Mirror")
-    buttons.buildbutton("Repo", "https://github.com/Adhil-AK/AK-Mirror-Leech-Bot")
-    buttons.buildbutton("Report Group", "https://t.me/Mltb_chat_unofficial")
+    buttons.buildbutton("Owner", "https://t.me/Lonewolfo")
+    buttons.buildbutton("Movies Group", "https://t.me/moviesondemandagain")
+   
+    
     reply_markup = InlineKeyboardMarkup(buttons.build_menu(2))
     if CustomFilters.authorized_user(update) or CustomFilters.authorized_chat(update):
         start_string = f'''
-Welcome! AK Mirror Leech Bot is ready for you | I can mirror all your links/Torrents to Google Drive and can leech to Telegram!
+Welcome! MOD Mirror Leech Bot is ready for you | I can mirror all your links/Torrents to Google Drive and can leech to Telegram!
 Type /{BotCommands.HelpCommand} to get a list of available commands.
 '''
         sendMarkup(start_string, context.bot, update.message, reply_markup)
